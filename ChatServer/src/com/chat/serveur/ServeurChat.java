fd4fd99fc96619a251b0fd4799b23aa3bd4662f2
carli <2178077@etu.cegepsth.qc.ca>
2025-10-24 11:12:03 -0400

    commit init

--- ChatServer/src/com/chat/serveur/ServeurChat.java
+++ ChatServer/src/com/chat/serveur/ServeurChat.java
@@ -104,8 +104,7 @@ public class ServeurChat extends Serveur {
             if (!cnx.getAlias().equals(aliasExpediteur)){
 
                 cnx.envoyer(aliasExpediteur + " >>" + str);
-                System.out.println(" testtt ");
-                System.out.println("test100");
+
             }
 
         }
